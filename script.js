c0ed65f70c2da632657310fa0e713b189b0d57d3
pradyumna1198 <92784304+pradyumna1198@users.noreply.github.com>
2023-08-20 17:54:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,3 +2,22 @@ window.promises = [];
 
 // Do not change the code above this
 // add your promises to the array `promises`
+for (let i = 0; i < 5; i++){
+	const randomTime = Math.floor(Math.random() * 5000) + 1000;
+	const promise = new Promise ((resolve)=> {
+		setTimeout(() => {
+			resolve(`Promise ${i+1} ${randomTime/1000} `);
+		},randomTime);
+	});
+	promises.push(promise);
+	}
+
+Promise.any(promises)
+.then((result)=> {
+	const outputDiv = document,getElementbyId("output");
+	outputDiv.textContent = result;
+	
+})
+.catch ((error ) =>){
+	console.error("All promises rejected : ", error);
+});
